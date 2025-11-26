d44893c506bde368215343474486d30ab104182b
richerdman <112634685+richerdman@users.noreply.github.com>
2025-11-26 13:26:25 +0000

    Homepage and board view with components constants and filestructure

---
+++ components/button.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet, ViewStyle, TextStyle} from "react-native";
+import { COLORS, SPACING, FONT_SIZES } from "../constants/theme";
+
+
+type ButtonProps = {
+    title: string;
+    onPress: () => void;
+    icon?: string;
+    style?: ViewStyle;
+};
+
+export default function Button({ title, onPress, icon, style }: ButtonProps) {
+    return (
+        <TouchableOpacity 
+            style={[styles.button, style]}
+            onPress={onPress}
+        >
+            <Text style={styles.buttonText}>
+                {icon && "${icon} "} {title}
+            </Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: COLORS.primary,
+        paddingVertical: SPACING.md,
+        paddingHorizontal: SPACING.lg,
+        borderRadius: 8,
+        minWidth: 100,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 4,
+        elevation: 3,
+    },
+    buttonText: {
+        color: COLORS.white,
+        fontSize: FONT_SIZES.medium,
+        fontWeight: "600",
+    },
+
+});
